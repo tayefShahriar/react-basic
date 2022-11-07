d3348e5acb6920231c3b20f7d226e0b81756e816
unknown <tayef12@gmail.com>
2022-11-07 13:19:57 -0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import {useState} from 'react'
 const App = ()=>{
-    const [number1,setNumber1] = useState(0)
-    const [number2,setNumber2] = useState(0)
-    const [result,setResult] = useState(0)
-    const addnumber=()=>{
-        const resultdata = parseInt(number1) + parseInt(number2)
-        setResult(resultdata)
-    }
+        const [number, setnumber] = useState(0)
     return(
         <div>
-           <input onChange={(e)=>setNumber1(e.target.value)} type="number" />
-           <input onChange={(e)=>setNumber2(e.target.value)} type="number" />
-           <button onClick={addnumber}>Add</button>
-           <h1>{number1}</h1>
-           <h1>{number2}</h1>
-           <h1>Result:{result}</h1>
+            <button onClick={()=>setnumber(number-1)}>-</button>
+            <h1>{number}</h1>
+            <button onClick={()=>setnumber(number+1)}>+</button>
         </div>
     )
 }
